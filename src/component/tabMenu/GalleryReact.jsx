8d6c8941bb1954f45ca616edff4272f3d55d3fe4
Bapita <bapitaroy14@gmail.com>
2021-04-29 17:32:14 +0530

    React dynamic menu for restaurant

---
+++ src/component/tabMenu/GalleryReact.jsx
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import "../tabMenu/tab.css";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Menu from "./Menu";
+
+const GalleryReact = () => {
+    const [items, setItems] = useState(Menu);
+
+    const filterItem = (ctgItem) => {
+        const updatedItems = Menu.filter((curElem) => {
+            return curElem.category === ctgItem;
+        })
+        setItems(updatedItems);
+    }
+    return(
+        <>
+            <h1 className="mt-5 text-center main-heading">Order your Favorite Dish</h1>
+            <hr />
+            <div className="menu-tabs container">
+                    <div className="menu-tab d-flex justify-content-around">
+                        <button className="btn btn-warning" onClick={()=> filterItem('breakfast')}>Breakfast</button>
+                        <button className="btn btn-warning" onClick={()=> filterItem('lunch')}>Lunch</button>
+                        <button className="btn btn-warning" onClick={()=> filterItem('evening')}>Evening</button>
+                        <button className="btn btn-warning" onClick={()=> filterItem('dinner')}>Dinner</button>
+                        <button className="btn btn-warning" onClick={()=> setItems(Menu)}>All</button>
+                    </div>                
+            </div>
+        {/* Main Item section starts here */}
+
+        <div className="mt4 container-fluid menu-items">
+            <div className="row">
+                <div className="col-11 mx-auto">
+                    <div className="row my-5">
+
+                        {
+                            items.map((elem) => {
+                                    const {id, name, image, description, price} = elem;
+                                    return(
+                                        <>
+                                            <div className="item1 col-12 col-md-6 col-lg-6 col-xl-4 my-5 mx-5">
+                                                <div className="row Item-inside">
+                                                {/* Menu Image */}
+                                                    <div className="col-12 col-md-12 col-lg-4 img-div">
+                                                        <img src={image} alt={name} className="img-fluid"/>
+                                                    </div>
+                                                {/* Menu Description */}
+                                                    <div className="col-12 col-md-12 col-lg-8">
+                                                        <div className="main-title pt-4 pb-3">
+                                                            <h1>{name}</h1>
+                                                            <p>{description}</p>
+                                                        </div>
+                                                        <div className="menu-price-book">
+                                                            <div className="price-book-divide d-flex justify-content-between">
+                                                                <h2>Price: {price}/-</h2>
+                                                                <a href="#">
+                                                                    <button className="btn btn-primary">Order Now</button>
+                                                                </a>
+                                                            </div>
+                                                            <p>**Prices may vary.</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </>
+                                    );
+                            })
+                        }
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default GalleryReact;
---
+++ src/component/tabMenu/Menu.jsx
@@ -0,0 +1,79 @@
+const Menu = [
+    {
+       id: 1,
+       image: "images/maggi.svg",
+       name:"maggi",
+       category: "breakfast",
+       price: "55₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+ 
+   {
+       id: 2,
+       image: "images/pakoda.svg",
+       name:"Sausage",
+       category: "evening",
+       price: "200₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+   {
+       id: 3,
+       image: "images/barbeque.svg",
+       name:"Barbeque",
+       category: "breakfast",
+       price: "500₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+    {
+       id: 4,
+       image: "../images/burger.svg",
+       name:"Burger",
+       category: "lunch",
+       price: "150₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+     {
+       id: 5,
+       image: "../images/cupcake.svg",
+       name:"Cup Cake",
+       category: "evening",
+       price: "80₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+      {
+       id: 6,
+       image: "../images/beer.svg",
+       name:"Strong Beer",
+       category: "dinner",
+       price: "280₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 7,
+       image: "../images/icecream.svg",
+       name:"Ice Cream",
+       category: "dinner",
+       price: "60₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 8,
+       image: "../images/juice.svg",
+       name:"Fruit Juice",
+       category: "lunch",
+       price: "260₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 9,
+       image: "../images/coffee.svg",
+       name:"Cream Coffee",
+       category: "evening",
+       price: "150₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+];
+
+
+
+export default Menu;
